8d417f45e69e65931943ce3c4966270b3c549221
Glen Sequeira <glensequeira8@gmail.com>
2018-03-04 19:34:13 -0500

    initial commit

---
+++ src/main/java/com/se/main/Controller/ProfilePageController.java
@@ -0,0 +1,71 @@
+/**
+ * 
+ */
+package com.se.main.Controller;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+
+/**
+ * @author Glen
+ *
+ */
+@Controller
+public class ProfilePageController {
+	
+	String accessKey;
+	
+	
+	String privateKey;
+		
+	@PostMapping(value = "/picupload")
+	public ModelAndView uploadFile(@RequestParam("file") MultipartFile image) {
+		ModelAndView profilepg = new ModelAndView();
+		accessKey = System.getenv("accessKey");
+
+		privateKey = System.getenv("privateKey");	
+		BasicAWSCredentials creds = new BasicAWSCredentials(accessKey,privateKey);
+		AmazonS3 amazonS3 = AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(creds))
+				.withRegion(Regions.US_EAST_2).build();
+		try {
+			PutObjectRequest putreq = new PutObjectRequest("myamazonbucketgs794734", image.getOriginalFilename(),
+					image.getInputStream(), new ObjectMetadata()).withCannedAcl(CannedAccessControlList.PublicRead);
+			amazonS3.putObject(putreq);
+			String imgurl = "http://" + "myamazonbucketgs794734" + ".s3.amazonaws.com/" + image.getOriginalFilename();
+			profilepg.addObject("image", imgurl);
+			profilepg.setViewName("profilePage");
+			return profilepg;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			profilepg.setViewName("errorPage");
+			return profilepg;
+		}
+	}	
+	
+	@GetMapping(value="/")
+	public ModelAndView renderPage() {
+		ModelAndView index=new ModelAndView();
+		index.setViewName("index");
+		return index;
+	}
+	
+	
+	
+}
---
+++ src/main/webapp/WEB-INF/jsp/profilePage.jsp
@@ -0,0 +1,52 @@
+<html lang="en">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport"
+	content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet"
+	href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+	integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+	crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="css/styles.css">
+<script
+	src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+	integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+	crossorigin="anonymous"></script>
+</head>
+<body>
+<div class="row">
+	<div class="container-fluid">
+	
+		<nav class="navbar-custom">
+    		<a class="navbar-brand" href="#">		<img src="logo.jpg" class="d-inline-block align-top" height="30" width="30">	&nbsp;&nbsp;	Glen's Media Library</a>
+    	</nav>
+		
+<!-- 		<nav class="navbar navbar-light bg-light">
+			<a class="navbar-brand" href="#"> 
+				<img src="logo.png" class="d-inline-block align-top" alt="" height="22" width="22">Glen's Media Library
+			</a>
+		</nav> -->
+	</div>
+	
+	<div class="col-2">
+		<img src="${image}" class="imageBox" height="300" width="300" />
+	</div>
+	<div class="col-5"><h2><b>Glen Sequeira</b></h2>
+		<h5>This is my live Library. Experience my talking photos. Enjoy !!</h5>
+	</div>
+</div>
+	
+
+	<!-- jQuery first, then Tether, then Bootstrap JS. -->
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+		integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+		crossorigin="anonymous"></script>
+	<script
+		src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+		integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+		crossorigin="anonymous"></script>
+</body>
+</html>
